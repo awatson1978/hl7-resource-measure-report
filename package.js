d9e417a54fc05c366a142081d1e53748f4c590c6
Abigail Watson <awatson1978@gmail.com>
2020-02-07 13:04:08 -0600

    resolving dependencies and binary build

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-measure-report',
-  version: '2.1.2',
+  version: '2.1.3',
   summary: 'HL7 FHIR Resource - MeasureReport',
   git: 'https://github.com/clinical-meteor/hl7-resource-measure-report',
   documentation: 'README.md'
